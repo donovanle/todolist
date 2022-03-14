07cec1d2485b5731d8ad7602962adea9372f2e19
donovanlem <kiloatw@gmail.com>
2022-03-14 16:39:15 -0600

    todo list

---
+++ todolist/src/App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/todo';
+import Displaytodo from './components/displaytodo';
+
+function App() {
+  const [listitems, setListitems] = useState([])
+
+  const handleNewTodo = (newTodo)=>{
+    setListitems([...listitems, newTodo])
+  }
+
+  const handleToggleComplete = (index) =>{
+    const updatedtodo = listitems.map((todos, i) => {
+      if (index===i){
+        todos.complete = !todos.complete;
+      }
+      return todos;
+    })
+    setListitems(updatedtodo)
+  }
+
+  const toDelete = (j) =>{
+    const removeTodo = [...listitems].filter((todo, i)=> i !== j)
+    setListitems(removeTodo)
+  }
+
+  return (
+    <div className="App">
+      <Todo onNewTodo={handleNewTodo}/>
+      <Displaytodo toDelete={toDelete} todos={listitems} handleToggleComplete={handleToggleComplete}/>
+    </div>
+  );
+}
+ 
+export default App;
